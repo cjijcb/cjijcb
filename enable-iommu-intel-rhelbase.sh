2d1837234d5a49d9497d69d6f5d34eebb0ee94c1
cjijcb <84954357+cjijcb@users.noreply.github.com>
2021-06-07 09:09:56 +0800

    Create enable-iommu-intel-rhelbase.sh

---
+++ enable-iommu-intel-rhelbase.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sed -E 's/^(GRUB_CMDLINE_LINUX)(.*) (intel_iommu=on|.*)\"/\1\2 intel_iommu=on"/' /etc/default/grub &&
+grub2-mkconfig -o /boot/grub2/grub.cfg
+echo 'rebooting this machine is required. Would you like to reboot now?[y/n]' 
+read YN
+YN=$(echo $YN | tr '[:upper:]' '[:lower:]')
+case $YN in
+    y) reboot ;;
+    yes) reboot ;;
+    n)  exit 0;;
+    no) exit 0;;
+    *)  exit 0;;
+esac
