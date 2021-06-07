c19d284a223e3e629907eb794a8687fe1f5e4ee0
cjijcb <84954357+cjijcb@users.noreply.github.com>
2021-06-07 09:17:12 +0800

    Update enable-iommu-intel-rhelbase.sh

--- enable-iommu-intel-rhelbase.sh
+++ enable-iommu-intel-rhelbase.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 sed -i.bak -E 's/^(GRUB_CMDLINE_LINUX)(.*) (intel_iommu=on|.*)\"/\1\2 intel_iommu=on"/' /etc/default/grub &&
 grub2-mkconfig -o /boot/grub2/grub.cfg &&
-echo 'rebooting this machine is required. Would you like to reboot now?[y/n]' &&
-read YN
+echo 'rebooting this machine is required. Would you like to reboot now?[y/n]' && read YN
 YN=$(echo $YN | tr '[:upper:]' '[:lower:]')
 case $YN in
     y) reboot ;;
