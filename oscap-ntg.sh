b5abf51db6df7ce5bdcd277498e209e259bfea8e
cjijcb <84954357+cjijcb@users.noreply.github.com>
2021-06-13 00:41:44 +0800

    Update oscap-ntg.sh

--- oscap-ntg.sh
+++ oscap-ntg.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m'
 yum -y install python3
 PY_PATH=$( whereis 'python3' | sed -E 's/.*[[:space:]](\/.+python3)[^.].*/\1/' )
 ln -s $PY_PATH /usr/bin/python 2> /dev/null
-yum -y install openscap openscap-utils scap-security-guide
+echo 'Installing OpenScap'
+    yum -qy install openscap openscap-utils scap-security-guide && \
+        echo -e "${GREEN}[SUCCESS]${NC}"
 mkdir -p /var/ossec/wodles/oscap/content
 cp /usr/share/xml/scap/ssg/content/*ds.xml /var/ossec/wodles/oscap/content/
-curl https://raw.githubusercontent.com/cjijcb/cjijcb/main/oscap > /var/ossec/wodles/oscap/oscap
-curl https://raw.githubusercontent.com/cjijcb/cjijcb/main/oscap.py > /var/ossec/wodles/oscap/oscap.py
-curl https://raw.githubusercontent.com/cjijcb/cjijcb/main/template_xccdf.xsl > /var/ossec/wodles/oscap/template_xccdf.xsl
-curl https://raw.githubusercontent.com/cjijcb/cjijcb/main/template_oval.xsl > /var/ossec/wodles/oscap/template_oval.xsl
+curl -s https://raw.githubusercontent.com/cjijcb/cjijcb/main/oscap > /var/ossec/wodles/oscap/oscap
+curl -s https://raw.githubusercontent.com/cjijcb/cjijcb/main/oscap.py > /var/ossec/wodles/oscap/oscap.py
+curl -s https://raw.githubusercontent.com/cjijcb/cjijcb/main/template_xccdf.xsl > /var/ossec/wodles/oscap/template_xccdf.xsl
+curl -s https://raw.githubusercontent.com/cjijcb/cjijcb/main/template_oval.xsl > /var/ossec/wodles/oscap/template_oval.xsl
 OSC_PATH='/var/ossec/etc/ossec.conf'
 sed -iE '/<.*wodle.\+open-scap/,/<\/.*wodle.*>/ d' $OSC_PATH
 WDL=$( grep -n --color=never '<.*/.*wodle.*>' $OSC_PATH | tail -1  | cut -f1 -d: )
