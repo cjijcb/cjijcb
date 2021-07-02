a99251d6a9aa9ed969f35d9ea583eb2500828e2e
cjijcb <84954357+cjijcb@users.noreply.github.com>
2021-07-02 16:46:19 +0800

    Create autoBridge.sh

---
+++ linux-net-bridge/autoBridge.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+IPV4=$( nmcli | sed -E -n "/: connected to/,/inet4/{/inet4/ s/.*inet4[[:space:]](.*)\/.*/\1/p}" ) && \
+PRFX=$( nmcli | sed -E -n "/: connected to/,/inet4/{/inet4/ s/.*inet4.*\/(.*)/\1/p}" ) && \
+GTWY=$( ip route | sed -E -n "s/default via (.*) dev.*/\1/p" ) && \
+DNS=$( nmcli | sed -E -n "/DNS configuration:/,/servers:/{/servers:/p}" | cut -d' ' -f2- | sed "s/[[:space:]]/,/g" ) && \
+ETH=$(  nmcli | sed -E -n "s/.*: connected to (.*)/\1/p" ) && \
+nmcli con add type bridge ifname bridge0 con-name bridge0 && \
+nmcli con mod bridge0 ipv4.addr ${IPV4}/${PRFX} && \
+nmcli con mod bridge0 ipv4.gateway ${GTWY} && \
+nmcli con mod bridge0 ipv4.method manual && \
+nmcli con mod bridge0 ipv4.dns ${DNS} && \
+nmcli con mpd bridge0 bridge.stp no && \
+nmcli con mod bridge0 connection.autoconnect-slaves yes && \
+nmcli con down ${ETH} && \
+nmcli con up bridge0
