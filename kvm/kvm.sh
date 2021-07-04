48d64ec025e01124639ecfabafcf72942aeecc56
cjijcb <84954357+cjijcb@users.noreply.github.com>
2021-07-04 16:42:05 +0800

    Update kvm.sh

--- kvm/kvm.sh
+++ kvm/kvm.sh
@@ -3,7 +3,7 @@ yum -y enable virt
 yum -y install qemu-kvm libvirt-client\* libvirt-daemon\* xorg-x11-xauth virt-install virt-manager virt-clone
 systemctl enable --now libvirtd
 systemctl restart libvirtd
-if ! grep -q -E "GRUB_CMDLINE_LINE=.*intel_iommu=on" /etc/default/grub; then
-sed -i -E "s/^[[:space:]]*(GRUB_CMDLINE_LINUX=)[[:space:]]*(.)(.*)[[:space:]]+(intel_iommu=on|.*)(\"|\')/\1\2\3 intel_iommu=on\2/" /etc/default/grub
-fi
+sed -i.bak -E "s/^[[:space:]]*(GRUB_CMDLINE_LINUX=.+)intel_iommu=[^[:space:]]+[[:space:]](.*)/\1\2/" /etc/default/grub && \
+sed -i -E "s/^[[:space:]]*(GRUB_CMDLINE_LINUX=.+)[[:space:]]intel_iommu=[^(\"|\')]+/\1/" /etc/default/grub && \
+sed -i -E "s/^[[:space:]]*(GRUB_CMDLINE_LINUX=)[[:space:]]*(.)(.*)(\"|\')/\1\2\3 intel_iommu=on\2/" /etc/default/grub
 grub2-mkconfig -o /boot/grub2/grub.cfg
