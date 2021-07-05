cd57c4838c88d007feb8f43c379ed51647534d1c
cjijcb <84954357+cjijcb@users.noreply.github.com>
2021-07-06 07:38:28 +0800

    Update freeipa-srvr.sh

--- ipa/freeipa-srvr.sh
+++ ipa/freeipa-srvr.sh
@@ -33,13 +33,12 @@ ipa-server-install \
 --forwarder=208.67.222.222 \
 --forwarder=208.67.220.220 \
 --mkhomedir \
---enable-compat <<EOF
+--enable-compat \
+--no-ntp <<EOF
 ${DEFAULT}
 ${DEFAULT}
 ${DEFAULT}
-yes
 ${DEFAULT}
-no
 yes
 EOF
 #
