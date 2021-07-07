43257efacfa87951d7a1df10d5bfc4de87729897
cjijcb <84954357+cjijcb@users.noreply.github.com>
2021-07-07 13:07:19 +0800

    Update zabbix5-agnt.sh

--- zabbix/zabbix5-agnt.sh
+++ zabbix/zabbix5-agnt.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo -n "Enter the Zabbix server IP:"
 read ZBXIP 
+echo -n "Set hostname for this Zabbix agent (e.g. ZBX01) or just press enter for autogenaration:"
+read AZBX_HOSTNAME
 rpm -Uvh https://repo.zabbix.com/zabbix/5.0/rhel/$(rpm -E %{rhel})/x86_64/zabbix-release-5.0-1.el$(rpm -E %{rhel}).noarch.rpm
 yum -y install zabbix-agent
 #
@@ -8,9 +10,14 @@ sed -i \
 "s/^Server=.*/Server=${ZBXIP}/; \
 s/^ServerActive=.*/ServerActive=${ZBXIP}/; \
 s/.*HostMetadata=.*/HostMetadata=Linux/; \
-s/.*HostnameItem=.*/HostnameItem=system.hostname/; \
-s/Hostname=[[:alpha:]].*/#&/" \
+s/.*HostnameItem=.*/HostnameItem=system.hostname/" \
 /etc/zabbix/zabbix_agentd.conf
+#
+if [[ -z "${AZBX_HOSTNAME}" ]]; then
+  sed -i "s/Hostname=[[:alpha:]].*/#&/" /etc/zabbix/zabbix_agentd.conf
+else
+  sed -i "s/Hostname=[[:alpha:]].*/Hostname=${AZBX_HOSTNAME}/" /etc/zabbix/zabbix_agentd.conf
+fi
 #
 sudo systemctl restart zabbix-agent
 sudo systemctl enable zabbix-agent
