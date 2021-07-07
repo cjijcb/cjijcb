0b91d382fd8a9600dbc4c1723b35df96a8396788
cjijcb <84954357+cjijcb@users.noreply.github.com>
2021-07-07 12:56:49 +0800

    Update zabbix5-agnt.sh

--- zabbix/zabbix5-agnt.sh
+++ zabbix/zabbix5-agnt.sh
@@ -4,9 +4,9 @@ read ZBXIP
 rpm -Uvh https://repo.zabbix.com/zabbix/5.0/rhel/$(rpm -E %{rhel})/x86_64/zabbix-release-5.0-1.el$(rpm -E %{rhel}).noarch.rpm
 yum -y install zabbix-agent
 #
-sed -i.bak \
-"s/^Server=.*/${ZBXIP}/; \
-s/^ServerActive=.*/${ZBXIP}/; \
+sed -i \
+"s/^Server=.*/Server=${ZBXIP}/; \
+s/^ServerActive=.*/ServerActive=${ZBXIP}/; \
 s/.*HostMetadata=.*/HostMetadata=Linux/; \
 s/.*HostnameItem=.*/HostnameItem=system.hostname/; \
 s/Hostname=[[:alpha:]].*/#&/" \
