e2c9891212b72e0d640de8b4b733ae8e8f028146
cjijcb <84954357+cjijcb@users.noreply.github.com>
2021-07-07 15:56:36 +0800

    Update zabbix5-https.sh

--- zabbix/zabbix5-https.sh
+++ zabbix/zabbix5-https.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-yum -y install mod_ssl
-mkdir -p /etc/httpd/ssl/private
-chmod 700 /etc/httpd/ssl/private
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/httpd/ssl/private/apache-selfsigned.key -out /etc/httpd/ssl/apache-selfsigned.crt <<EOF
+GRN='\033[1;32m'
+NC='\033[0m'
+IPV4=$( nmcli | sed -E -n "/: connected to/,/inet4/{/inet4/ s/.*inet4[[:space:]](.*)\/.*/\1/p}" ) && \
+yum -y install mod_ssl && \
+mkdir -p /etc/httpd/ssl/private && \
+chmod 700 /etc/httpd/ssl/private && \
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/httpd/ssl/private/apache-selfsigned.key -out /etc/httpd/ssl/apache-selfsigned.crt > /dev/null 2>&1 <<EOF || exit
 ${NULL}
 ${NULL}
 ${NULL}
@@ -13,8 +16,9 @@ ${NULL}
 EOF
 sed -i \
 "s/^DocumentRoot.*/DocumentRoot \"\/usr\/share\/zabbix\"/; \
-s/^ServerName.*/ServerName 127.0.0.1:443/; \
+s/^ServerName.*/ServerName ${IPV4}:443/; \
 s/^SSLCertificateFile.*/SSLCertificateFile \/etc\/httpd\/ssl\/apache-selfsigned.crt/; \
 s/^SSLCertificateKeyFile.*/SSLCertificateKeyFile \/etc\/httpd\/ssl\/private\/apache-selfsigned.key/" \
-/etc/httpd/conf.d/ssl.conf
-systemctl restart httpd
+/etc/httpd/conf.d/ssl.conf && \
+systemctl restart httpd && \
+echo -e "${GRN}HTTPS successfully enabled.${NC}"
