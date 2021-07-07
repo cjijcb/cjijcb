c5481886269953f7af89d966d09a783d86dd8892
cjijcb <84954357+cjijcb@users.noreply.github.com>
2021-07-08 07:24:49 +0800

    Create tacacs.sh

---
+++ tacacs/tacacs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+yum -y install \
+libnsl \
+perl \
+https://pkgs.dyn.su/el8/extras/x86_64/tcp_wrappers-libs-7.6-77.el8.x86_64.rpm \
+http://li.nux.ro/download/nux/misc/el7/x86_64/tac_plus-4.0.4.26-1.el7.nux.x86_64.rpm \
+http://li.nux.ro/download/nux/misc/el7/x86_64/tac_plus-debuginfo-4.0.4.26-1.el7.nux.x86_64.rpm \
+http://li.nux.ro/download/nux/misc/el7/x86_64/tac_plus-devel-4.0.4.26-1.el7.nux.x86_64.rpm && \
+service tac_plus start
+systemctl enable tac_plus
