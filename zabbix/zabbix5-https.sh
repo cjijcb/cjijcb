2ba53c90f2083c16efe4487c1c76d3920e389f3a
cjijcb <84954357+cjijcb@users.noreply.github.com>
2021-07-07 15:47:00 +0800

    Create zabbix5-https.sh

---
+++ zabbix/zabbix5-https.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+yum install mod_ssl
+mkdir -p /etc/httpd/ssl/private
+chmod 700 /etc/httpd/ssl/private
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/httpd/ssl/private/apache-selfsigned.key -out /etc/httpd/ssl/apache-selfsigned.crt <<EOF
+${NULL}
+${NULL}
+${NULL}
+${NULL}
+${NULL}
+$(hostname)
+${NULL}
+EOF
+sed -i \
+"s/^DocumentRoot.*/DocumentRoot \"\/usr\/share\/zabbix\"/; \
+s/^ServerName.*/ServerName 127.0.0.1:443/; \
+s/^SSLCertificateFile.*/SSLCertificateFile \/etc\/httpd\/ssl\/apache-selfsigned.crt/; \
+s/^SSLCertificateKeyFile.*/SSLCertificateKeyFile \/etc\/httpd\/ssl\/private\/apache-selfsigned.key/" \
+/etc/httpd/conf.d/ssl.conf
+systemctl restart httpd
