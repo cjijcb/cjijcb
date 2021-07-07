0e712028dce7e8d73edd5ded4d9f9e74440d8f2c
cjijcb <84954357+cjijcb@users.noreply.github.com>
2021-07-07 12:47:06 +0800

    Update zabbix5-agnt.sh

--- zabbix/zabbix5-agnt.sh
+++ zabbix/zabbix5-agnt.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
+echo -n "Enter the Zabbix server IP:"
+read ZBXIP 
 rpm -Uvh https://repo.zabbix.com/zabbix/5.0/rhel/$(rpm -E %{rhel})/x86_64/zabbix-release-5.0-1.el$(rpm -E %{rhel}).noarch.rpm
 #
-sed "s/^Server=.*/pooooooooooooooooo/" /etc/zabbix/zabbix_agentd.conf
-sed "s/^ServerActive=.*/pooooooooooooooooo/" /etc/zabbix/zabbix_agentd.conf
-sed "s/.*HostMetadata=.*/HostMetadata=Linux/" /etc/zabbix/zabbix_agentd.conf
-sed "s/.*HostnameItem=.*/HostnameItem=system.hostname/" /etc/zabbix/zabbix_agentd.conf
-sed "s/Hostname=[[:alpha:]].*/#&/" /etc/zabbix/zabbix_agentd.conf
+sed -i.bak \
+"s/^Server=.*/${ZBXIP}/; \
+s/^ServerActive=.*/${ZBXIP}/; \
+s/.*HostMetadata=.*/HostMetadata=Linux/; \
+s/.*HostnameItem=.*/HostnameItem=system.hostname/; \
+s/Hostname=[[:alpha:]].*/#&/" \
+/etc/zabbix/zabbix_agentd.conf
+#
 sudo systemctl restart zabbix-agent
 sudo systemctl enable zabbix-agent
-#
