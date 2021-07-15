a5763335543877977a2d96fc443cffd401a946aa
cjijcb <84954357+cjijcb@users.noreply.github.com>
2021-07-16 07:58:38 +0800

    Update ol8-hardening.sh

--- ol8-hardening.sh
+++ ol8-hardening.sh
@@ -12,6 +12,7 @@ systemctl unmask tmp.mount
 systemctl enable tmp.mount
 sed -i 's/^Options=.*/Options=mode=1777,strictatime,noexec,nodev,nosuid/' /etc/systemd/system/local-fs.target.wants/tmp.mount
 systemctl start tmp.mount
+echo 'systemctl start tmp.mount' >> /etc/rc.local
 #5007
 echo 'tmpfs /dev/shm tmpfs defaults,nodev,nosuid,noexec 0 0' >> /etc/fstab
 mount -o remount,noexec /tmp
@@ -40,4 +41,56 @@ echo 'kernel.randomize_va_space = 2' >> /etc/sysctl.d/randomize-space.conf
 sysctl -w kernel.randomize_va_space=2
 #5088
 echo 'install tipc /bin/true' >> /etc/modprobe.d/tipc.conf
+#5042
+echo 'Authorized uses only. All activity may be monitored and reported.' > /etc/issue
+#5043
+echo 'Authorized uses only. All activity may be monitored and reported.' > /etc/issue.net
+#5050
+update-crypto-policies --set FIPS
+fips-mode-setup --enable
+#5075
+cho -e \
+"net.ipv4.conf.all.send_redirects = 0\n\
+net.ipv4.conf.default.send_redirects = 0" \
+>> /etc/sysctl.conf
+sysctl -w net.ipv4.conf.all.send_redirects=0
+sysctl -w net.ipv4.conf.default.send_redirects=0
+sysctl -w net.ipv4.route.flush=1
+#5076
+echo -e \
+"net.ipv4.conf.all.accept_source_route = 0\n\
+net.ipv4.conf.default.accept_source_route = 0\n\
+net.ipv6.conf.all.accept_source_route = 0\n\
+net.ipv6.conf.default.accept_source_route = 0" \
+>> /etc/sysctl.conf
+sysctl -w net.ipv4.conf.all.accept_source_route=0
+sysctl -w net.ipv4.conf.default.accept_source_route=0
+sysctl -w net.ipv6.conf.all.accept_source_route=0
+sysctl -w net.ipv6.conf.default.accept_source_route=0
+sysctl -w net.ipv4.route.flush=1
+sysctl -w net.ipv6.route.flush=1
+#5077
+echo -e \
+"net.ipv4.conf.all.accept_redirects = 0\n\
+net.ipv4.conf.default.accept_redirects = 0\n\
+net.ipv6.conf.all.accept_redirects = 0\n\
+net.ipv6.conf.default.accept_redirects = 0" \
+>> /etc/sysctl.conf
+sysctl -w net.ipv4.conf.all.accept_redirects=0
+sysctl -w net.ipv4.conf.default.accept_redirects=0;
+sysctl -w net.ipv6.conf.all.accept_redirects=0;
+sysctl -w net.ipv6.conf.default.accept_redirects=0;
+sysctl -w net.ipv4.route.flush=1
+sysctl -w net.ipv6.route.flush=1
+#5078
+echo -e \
+"net.ipv4.conf.all.secure_redirects = 0\n\
+net.ipv4.conf.default.secure_redirects = 0" \
+>> /etc/sysctl.conf
+sysctl -w net.ipv4.conf.all.secure_redirects=0
+sysctl -w net.ipv4.conf.default.secure_redirects=0
+sysctl -w net.ipv4.route.flush=1
+
+
+
 
