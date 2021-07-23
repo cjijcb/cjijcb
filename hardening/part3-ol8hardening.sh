60fcf9fd05b46c048d2ebaab3959556ede56f351
cjijcb <84954357+cjijcb@users.noreply.github.com>
2021-07-23 08:27:43 +0800

    Update part3-ol8hardening.sh

--- hardening/part3-ol8hardening.sh
+++ hardening/part3-ol8hardening.sh
@@ -78,7 +78,7 @@ yum -y install psacct
 systemctl enable psacct
 systemctl start psacct
 #
-yum -y install epel-release
+yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
 yum -y install haveged
 systemctl enable haveged
 systemctl start haveged
