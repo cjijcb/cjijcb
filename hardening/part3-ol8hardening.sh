46e3888acab4a766f8668b9b12b36c89337bed56
cjijcb <84954357+cjijcb@users.noreply.github.com>
2021-07-23 08:24:28 +0800

    Update part3-ol8hardening.sh

--- hardening/part3-ol8hardening.sh
+++ hardening/part3-ol8hardening.sh
@@ -84,8 +84,12 @@ systemctl enable haveged
 systemctl start haveged
 #
 yum -y install rkhunter
+systemctl enable rkhunter
+systemctl start rkhunter
 #
 yum -y install ansible
+systemctl enable ansible
+systemctl start ansible
 #
 sed -i -E "s/([^[:space:]]+[[:space:]]+\/boot[[:space:]]+[^[:space:]]+[[:space:]]+[^[:space:]]+)([[:space:]]+.*)/\1,nodev,nosuid,noexec,ro\2/" /etc/fstab
 mount -o remount,rw,nodev,nosuid,noexec /boot
