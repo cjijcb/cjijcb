bb3dd1531523a4862a914c3a6bba5980f162734f
cjijcb <84954357+cjijcb@users.noreply.github.com>
2021-10-27 11:11:29 +0800

    Create rescueME.sh

---
+++ rescueME.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+GRN='\033[1;32m'
+NC='\033[0m'
+BOOL=0
+RESCUE_ACCOUNT=eucser
+PASSWORD='{f>e,o;I0.tL2T#LGMD<^h|xdXx9/5e;Pk@z7h-3}AwF\){"/?6G."{XM~fFqnX'
+echo "creating rescue account..."
+while [ ${BOOL} -eq 0 ]
+do
+  if ! sudo useradd -M \
+       -G wheel \
+       -K PASS_MAX_DAYS=365 \
+       -K PASS_WARN_AGE=3 \
+       -K LOGIN_RETRIES=3 ${RESCUE_ACCOUNT} > /dev/null 2>&1 && break
+  then
+     sudo pkill ${RESCUE_ACCOUNT} &&
+     sudo userdel ${RESCUE_ACCOUNT}
+  fi
+done
+sudo passwd ${RESCUE_ACCOUNT} <<EOF > /dev/null 2>&1 || exit
+${PASSWORD}
+${PASSWORD}
+EOF
+#
+echo "disabling root account..."
+sudo sed -E -i "s/(^root[^\/]+\/root:).*/\1\/sbin\/nologin/" /etc/passwd &&
+sudo sed -E -i "s/^[#]*(PermitRootLogin[[:space:]]+)[[:alpha:]]+/\1no/" /etc/ssh/sshd_config &&
+sudo systemctl restart sshd &&
+echo -e "${GRN}Success!${NC}"
